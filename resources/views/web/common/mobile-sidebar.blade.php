33fd5fcee083f890bb3dc1a5d31d39accebee126
Labhansh Satpute <73733885+labhanshsatpute@users.noreply.github.com>
2023-04-09 22:56:11 +0530

    changes

--- resources/views/web/common/mobile-sidebar.blade.php
+++ resources/views/web/common/mobile-sidebar.blade.php
@@ -1,6 +1,50 @@
 <aside class="sidebar" id="sidebar">
     <div class="p-7 pt-[95px]">
         <ul class="flex flex-col justify-center">
+
+            <li class="border-b">
+                <a href="{{route('view.dashboard')}}">
+                    <button class="w-full py-5 text-left">
+                        <div class="flex items-center justify-between font-medium text-base">
+                            My Acoount
+                        </div>
+                    </button>
+                </a>
+            </li>
+
+            <li class="border-b">
+                <a href="{{route('view.schedule.shipment')}}">
+                    <button class="w-full py-5 text-left">
+                        <div class="flex items-center justify-between font-medium text-base">
+                            Schedule a Shipment
+                        </div>
+                    </button>
+                </a>
+            </li>
+
+            @auth
+            <li class="border-b">
+                <a href="{{route('view.shipments')}}">
+                    <button class="w-full py-5 text-left">
+                        <div class="flex items-center justify-between font-medium text-base">
+                            My Shipments
+                        </div>
+                    </button>
+                </a>
+            </li>
+
+            <li class="border-b">
+                <a href="{{route('view.setting')}}">
+                    <button class="w-full py-5 text-left">
+                        <div class="flex items-center justify-between font-medium text-base">
+                            Account Setting
+                        </div>
+                    </button>
+                </a>
+            </li>
+            @endauth
+
+            
             
             <li class="border-b">
                 <a href="{{--route('view.dashboard')--}}">
@@ -43,26 +87,6 @@
             </li>
 
             <li class="border-b">
-                <a href="{{route('view.schedule.shipment')}}">
-                    <button class="w-full py-5 text-left">
-                        <div class="flex items-center justify-between font-medium text-base">
-                            Schedule a Shipment
-                        </div>
-                    </button>
-                </a>
-            </li>
-
-            <li class="border-b">
-                <a href="{{route('view.dashboard')}}">
-                    <button class="w-full py-5 text-left">
-                        <div class="flex items-center justify-between font-medium text-base">
-                            My Acoount
-                        </div>
-                    </button>
-                </a>
-            </li>
-
-            <li class="border-b">
                 <a href="{{--route('view.dashboard')--}}">
                     <button class="w-full py-5 text-left">
                         <div class="flex items-center justify-between font-medium text-base">
@@ -71,6 +95,18 @@
                     </button>
                 </a>
             </li>
+
+            @auth
+            <li class="border-b">
+                <a href="javascript:handleLogout();">
+                    <button class="w-full py-5 text-left">
+                        <div class="flex items-center justify-between font-medium text-base">
+                            Logout
+                        </div>
+                    </button>
+                </a>
+            </li>
+            @endauth
 
             <li class="border-b py-5">
                 <a href="{{--route('view.dashboard')--}}">
